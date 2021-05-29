fd329e8367394e9c301aba1fb4e937ed0b8198a9
Varshini <varshini@123>
2021-05-29 21:24:13 +0530

    evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,11 @@
+class evenodd
+{
+public static void main(String args)
+{
+int n;
+if(n%2==0)
+System.out.println("n is even");
+else
+System.out.println("n is odd");
+}
+}
---
+++ multiplication.java
@@ -0,0 +1,9 @@
+class multiplication
+{
+public static void main(String args)
+{
+int a,b;
+c=a*b;
+System.out.println("product of a and b: "||c);
+}
+}
